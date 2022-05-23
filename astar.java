1d958deca0796ac4e1bbdd8bd6a1c6bbff92cce5
NzWildthing <brogan131001@gmail.com>
2022-05-23 16:29:27 +1200

    algorithm working and drawing

--- astar.java
+++ astar.java
@@ -15,6 +15,9 @@ public class astar extends JPanel {
 
     public static ArrayList<Node> coordinates = new ArrayList<>();
 
+    //Holds the stack of the final path 
+    public static Stack<Node> path = new Stack<>();
+
     //Saves the specified distance at the start 
     public static double dist;
 
@@ -28,6 +31,7 @@ public class astar extends JPanel {
         public double heuristic = 0; 
         public double cost = 0; 
         public double f_value = 0;
+        public Node prev;
         
         //Array list of nodes that are within range of current node based off distance
         public ArrayList<Node> nodesinRange = new ArrayList<>();
@@ -42,21 +46,22 @@ public class astar extends JPanel {
 
     public static void main(String[] args){
         try{
-            //java Stars [galaxy_csv_filename] [start_index] [end_index] [D]
+            //checks input is in correct format with right number of entries
             if(args.length != 4) {
 
-                System.err.println("");
+                System.err.println("Invalid Input- Please use format below");
+                System.err.println("java Stars [galaxy_csv_filename] [start_index] [end_index] [D]");
             }
 
-            //Read in coordinates 
+            //Read in CSV file
             FileReader file = new FileReader(args[0]);
             BufferedReader reader = new BufferedReader(file);
             String line = reader.readLine();
-
+            //save args as global varibles
             starti= Integer.parseInt(args[1]);
             endi= Integer.parseInt(args[2]);
             dist = Double.parseDouble(args[3]);
-
+            //declaring coordinate values
             double x, y;
 
             //find start and end
@@ -79,16 +84,19 @@ public class astar extends JPanel {
                     endy=y;
                     }
                 
-                System.out.println(x + "     " + y);
-                
                 line = reader.readLine();
             }
-            
             reader.close();
 
             System.out.println("Start: " + startx + " " + starty);
             System.out.println("End: " + endx + " " + endy);
+        }
+        catch(Exception e){
+            System.err.println("Error occurred while extracting CSV information. Try again");
+        }
+        try{
 
+        
             //Precompute what nodes are within distance to each other for easy traversal
             preCalculateRange();
 
@@ -100,14 +108,12 @@ public class astar extends JPanel {
 
             //Draws the graphical GUI
             drawGraph();
-        
-
 
         
         }	
-        catch(Exception eAStar) {
+        catch(Exception e) {
 
-			System.err.println("Error: " + eAStar);
+			System.err.println("Error: " + e);
             
 		}
 
@@ -116,9 +122,10 @@ public class astar extends JPanel {
 
     //Drawing graph/gui
     public static void drawGraph(){                        
-        
+            //create graphics frame to draw map onto
             astar gui = new astar(); 
 			JFrame frame = new JFrame("The Starship Enterprise's Mystical Journey Through The Cosmos");
+
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setSize(800,750);
             frame.setLocation(200,200);
@@ -132,15 +139,13 @@ public class astar extends JPanel {
         int width=5, height = 5, margin=100, size=710, count=0;
         Double x=0.0, y=0.0;    
         Graphics2D g1=(Graphics2D)g;
-        //draw axis aroud stars
-        g1.draw(new Line2D.Double(margin, margin-10, margin, size-margin+20));
-        g1.draw(new Line2D.Double(margin, size-margin+20, size, size-margin+20));
-
+        //draw box around stars
+        g1.draw(new Line2D.Double(margin, 90, margin, 630));
+        g1.draw(new Line2D.Double(margin, 630, size, 630));
+        g1.draw(new Line2D.Double(size, 90, size, 630));
+        g1.draw(new Line2D.Double(margin, 90, size, 90));
         
-        g1.draw(new Line2D.Double(size, margin-10, size, size-margin+20));
-        g1.draw(new Line2D.Double(margin, margin-10, size, margin-10));
-        
-        
+        //create a font to draw labels
         Font font = new Font("Arial", Font.BOLD, 16);
         g1.setFont(font);
 
@@ -159,23 +164,22 @@ public class astar extends JPanel {
         
         g1.setPaint(Color.BLUE);
         g1.drawString("Star", 5, 80);
-        g1.fill(new Ellipse2D.Double(100, 70, width+3, height+3));
+        g1.fill(new Ellipse2D.Double(100, 70, width, height));
         
-        
-        //run through all stars
+      
+        //run through and draw all stars
         for(Node node : stars){
-            //get drawing coordinates 
-            Double lastx=x;
-            Double lasty=y;
-            x = node.x_coord *6;
-            y = node.y_coord *6;
+
+            //get coordinates and scale for better visualization
+            x = (node.x_coord *6);
+            y = (node.y_coord *6);
+
             //set stars colours
             g1.setPaint(Color.BLUE);
             // if start point
             if(x/6==startx && y/6==starty){
                 //set stars colours
                 g1.setPaint(Color.GREEN);
-                
                 //draw starting star
                 g1.fill(new Ellipse2D.Double(x+margin,y+margin, width+3, height+3));
             }//else if end point 
@@ -189,13 +193,27 @@ public class astar extends JPanel {
             else{
                 //draw star
                 g1.fill(new Ellipse2D.Double(x+margin,y+margin, width, height));
-            }
-            g1.setPaint(Color.PINK);
-            //if not the 1st point
-            if(count!=0){g1.draw(new Line2D.Double(lastx+margin+2.5, lasty+margin+2.5, x+margin+2.5, y+margin+2.5));}
-            count+=1;
+            }    
             
-        }     
+        }  
+        //
+        //draw path to goal
+        for (Node nodes : path){
+            //save previous start coordinates
+            Double lastx=x;
+            Double lasty=y;
+            //get coordinates and scale for better visualization
+            x = (nodes.x_coord *6);
+            y = (nodes.y_coord *6);
+            System.out.println("the size of path is " + path.size());
+            //set to path colour
+            g1.setPaint(Color.PINK);
+            //if not the 1st point draw path between points
+            if(count!=0){g1.draw(new Line2D.Double(lastx+2.5+margin, lasty+2.5+margin, x+2.5+margin, y+2.5+margin));}
+            count+=1;   
+
+
+        }
     
     }
 
@@ -237,42 +255,80 @@ public class astar extends JPanel {
         
         //Makes the frontier to look through 
         ArrayList<Node> frontier = new ArrayList<>();
-        Stack<Node> theStack = new Stack<>();
-
+        //Holds the previously tried node
+        Node prev_tryNode = null;
+        Node startNode = stars.get(starti-1);
         //Initialises the start of the frontier with the start node 
-        frontier.add(stars.get(starti + 1));
-
-        while(!frontier.isEmpty()){
-            Collections.sort(frontier, new Comparator<Node>(){
-                public int compare(Node o1, Node o2){
-                    if(o1.f_value == o2.f_value)
-                        return 0;
-                    return o1.f_value< o2.f_value ? -1 : 1;
+        frontier.add(startNode);
+        try{
+            while(!frontier.isEmpty()){
+                Collections.sort(frontier, new Comparator<Node>(){
+                    public int compare(Node o1, Node o2){
+                        if(o1.f_value == o2.f_value)
+                            return 0;
+                        return o1.f_value< o2.f_value ? -1 : 1;
+                    }
+                });
+                //Gets the head of the frontier 
+                Node curr = frontier.get(0);
+                frontier.remove(0);
+                //Check if the node in the frontier was tried before and then gets another node if so to stop looping
+                if(prev_tryNode == curr){
+                    curr = frontier.get(0);
+                    frontier.remove(0);
                 }
-            });
-            //Gets the head of the frontier 
-            Node n = frontier.get(0);
-            frontier.remove(0);
-            //Initally checks if the node pulled off the top is the goal 
-            if(n.x_coord == endx && n.y_coord == endy){
-                //Found the route 
-                //Gets the route from the stack that can then be drawn between 
-                System.out.println("FOUND THE GOAL!!!");
-                return;
+                //Sets the previous node to the current 
+                prev_tryNode = curr;
+                //Initally checks if the node pulled off the top is the goal 
+                if(curr.x_coord == endx && curr.y_coord == endy){
+                    //Found the route 
+                    //Gets the route from the stack that can then be drawn between 
+                    System.out.println("Path to goal:");
+                     
+                    //Loops through the full list of stars backtracking through the end goals previous 
+                    while (curr != null && curr != stars.get(starti-1)) {
+                        //And adding it to the path 
+                        path.add(curr);
+                        //Then moves onto the next node until the end of the path has been gathered 
+                        curr = curr.prev;
+                    }
+                    path.add(curr);
+
+                    return;
+                }
+                //Get the next paths from that node and places into frontier  
+                for(Node node: curr.nodesinRange){
+                    //Caclulates the cost value for the new node 
+                    double cost = node.cost + calculateDistanceBetweenPoints(curr.x_coord, curr.y_coord, node.x_coord, node.y_coord);
+                    //Calculates the new nodes f value r
+                    double f_value = calcfValue(node);
+                    //If the calculated cost is less than the nodes current cost 
+                    if (cost < node.cost || node.cost == 0) {
+                        //Updates the nodes previous node
+                        node.prev = curr;
+                        //Updates the cost and f value of the current node 
+                        node.cost = cost;
+                        node.f_value = f_value;
+                        //If node is not in the frontier currently 
+                        if (!frontier.contains(node)) {
+                            frontier.add(node);
+                        }
+                    }
+                }
+                //Places that node expanded out onto the stack 
+                //path.add(curr);
             }
-            //Get the next paths from that node and places into frontier  
-            for(Node node: n.nodesinRange){
-                //Caclulates the cost value for the new node 
-                node.cost = node.cost + calculateDistanceBetweenPoints(n.x_coord, n.y_coord, node.x_coord, node.y_coord);
-                //Calculates the new nodes f value then places into the frontier 
-                node.f_value = calcfValue(node);
-                frontier.add(node);
-            }
-            //Places that node expanded out onto the stack 
-            theStack.add(n);
+        //catches of the algorithm does not find the goal star
+        }catch(Exception e){
+            System.err.println("No Route Found");
         }
-        
     }
+
+    //Prints out the goal from the stack 
+    //while(!path.isEmpty()){
+    //    Node n = (Node)path.pop();
+    //    System.out.println(n.x_coord + " " + n.y_coord);
+    //}
 
     //Loops through each of the nodes presetting their heuristics
     public static void setHeuristic(){
